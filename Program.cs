a37139c3e4802cf5882a10c18630c760b600e172
cympfh <cympfh@gmail.com>
2021-12-02 17:29:30 +0900

    :new: mousemove, click, mousedown, mouseup

--- Program.cs
+++ Program.cs
@@ -3,13 +3,14 @@ using Microsoft.Extensions.Hosting;
 using System.Windows.Forms;
 using System;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace windotool
 {
     class Program : ConsoleAppBase
     {
-        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
-        static extern bool AttachConsole( int dwProcessId );
+        [DllImport("kernel32.dll")]
+        static extern bool AttachConsole(int dwProcessId);
 
         /// <summary>
         /// windotool Entrypoint
@@ -30,6 +31,89 @@ namespace windotool
             if (verbose)
             {
                 Console.WriteLine($"Sent Key: {key}");
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(long dwFlags, long dx, long dy, uint dwData, UIntPtr dwExtraInfo);
+
+        [Flags]
+        public enum MouseEvent : long
+        {
+            Move = 0x0001,
+            LeftDown = 0x0002,
+            LeftUp = 0x0004,
+            RightDown = 0x0008,
+            RightUp = 0x0010,
+            MiddleDown = 0x0020,
+            MiddleUp = 0x0040,
+            Absolute = 0x8000
+        }
+
+        [Command("mousemove")]
+        public void MouseMove(
+            [Option(0)] long x,
+            [Option(1)] long y,
+            [Option("v")] bool verbose = false
+        )
+        {
+            var scr = Screen.PrimaryScreen.Bounds;
+            var normalized_x = (long)Math.Round((float)(x) * 65535 / scr.Width);
+            var normalized_y = (long)Math.Round((float)(y) * 65535 / scr.Height);
+            mouse_event((long)(MouseEvent.Move | MouseEvent.Absolute), normalized_x, normalized_y, 0, UIntPtr.Zero);
+            if (verbose)
+            {
+                Console.WriteLine($"Moved to {x} {y}");
+            }
+        }
+
+
+        [Command("mousedown")]
+        public void MouseDown(
+            [Option(0)] uint button,
+            [Option("v")] bool verbose = false
+        )
+        {
+            var e = MouseEvent.LeftDown;
+            if (button == 2) { e = MouseEvent.MiddleDown; }
+            else if (button == 3) { e = MouseEvent.RightDown; }
+            mouse_event((long)(e), 0, 0, 0, UIntPtr.Zero);
+            if (verbose)
+            {
+                Console.WriteLine($"MouseDown: {button}");
+            }
+        }
+
+
+        [Command("mouseup")]
+        public void MouseUp(
+            [Option(0)] uint button,
+            [Option("v")] bool verbose = false
+        )
+        {
+            var e = MouseEvent.LeftUp;
+            if (button == 2) { e = MouseEvent.MiddleUp; }
+            else if (button == 3) { e = MouseEvent.RightUp; }
+            mouse_event((long)(e), 0, 0, 0, UIntPtr.Zero);
+            if (verbose)
+            {
+                Console.WriteLine($"MouseUp: {button}");
+            }
+        }
+
+        [Command("click")]
+        public void Click(
+            [Option(0)] uint button,
+            [Option("v")] bool verbose = false
+        )
+        {
+            var e = MouseEvent.LeftDown | MouseEvent.LeftUp;
+            if (button == 2) { e = MouseEvent.MiddleDown | MouseEvent.MiddleUp; }
+            else if (button == 3) { e = MouseEvent.RightDown | MouseEvent.RightUp; }
+            mouse_event((long)(e), 0, 0, 0, UIntPtr.Zero);
+            if (verbose)
+            {
+                Console.WriteLine($"Clicked: {button}");
             }
         }
     }
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # windotool -- xdotool for Windows Desktop
 
+## Install
+
+TBW.
+Executable binary will be released...
+
 ## Usage
 
 See
@@ -8,17 +13,53 @@ See
 $ windotool help
 ```
 
-None
+### key
+
+```bash
+$ windotool key Hello  # Types H, e, l, l and o
+$ windotool key "^D"   # Ctrl+Shift+D
+```
+
+For details for key notation,
+please see [.NET document](https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys?view=windowsdesktop-6.0).
+
+### mousemove
+
+```bash
+$ windotool mousemove {x} {y}  # absolute point
+$ windotool mousemove 200 100
+```
+
+### click
+
+```bash
+$ windotool click {button}  # left is 1, middle is 2 and right is 3.
+$ windotool click 1
+```
+
+Note: "click" is "mousedown then mouseup".
+
+### mousedown, mouseup
+
+See click.
+
+```bash
+# Select (100,100) to (200,200) by leftbutton
+$ windotool movemove 100 100
+$ windotool mousedown 1
+$ windotool movemove 200 200
+$ windotool mouseup 1
+```
 
 ## Todo
 
-- [ ] key
+- [x] key
 - [ ] keydown
 - [ ] keyup
-- [ ] click
-- [ ] mousedown
-- [ ] mouseup
-- [ ] mousemove
+- [x] mousemove
+- [x] mousedown
+- [x] mouseup
+- [x] click
 
 ## For Windows Developers
 
